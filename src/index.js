4ac437ceaeb7f10d8bd5bfb37ec0366b474f6a5b
Brian Detering <brian@tourmalinelabs.com>
2018-06-07 18:10:47 -0700

    fix flow type

--- src/index.js
+++ src/index.js
@@ -158,7 +158,7 @@ const createSqlWrap = ({
 
   self.build = (): * => query.build();
 
-  self.escape = (data: mixed): mixed => sqlString.escape(data);
+  self.escape = (data: string): string => sqlString.escape(data);
   self.escapeId = (data: string): string => sqlString.escapeId(data);
 
   self.encodeCursor = (orderBy: *, row: *): * =>
